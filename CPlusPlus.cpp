0a80d3a4d8a6c5bbbf1d1cf979da2376ef15e2a8
SnowballSH <derek070211@gmail.com>
2020-10-29 20:35:54 -0500

    a lot

---
+++ CPlusPlus.cpp
@@ -0,0 +1,6 @@
+#include <iostream.h>
+main()
+{
+  cout << "Hello World!" << endl;
+  return 0;
+}
---
+++ CSharp.cs
@@ -0,0 +1,7 @@
+class Main
+{
+  static void Main()
+  {
+    System.Console.WriteLine("Hello, world!");
+  }
+}
